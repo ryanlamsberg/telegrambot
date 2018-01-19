cd519aba7425b49ba4442c3efbe15e8c86cc875c
ryanlamsberg <ryan.lamsberg@coderclass.nl>
2018-01-19 11:09:05 +0100

    eerste versie

---
+++ Desktop/Coderclass/Telegram Bot/TelegramBot.js
@@ -0,0 +1,26 @@
+
+const TeleBot = require('telebot');
+
+const bot = new TeleBot({
+    token: '468814688:AAGjPJ_irCzgB6iC4CcFgz4gCdVVV1eOOmk'
+});
+
+bot.on(/start/, function (msg) {
+  return bot.sendMessage(msg.from.id, "Hello world!");
+});
+bot.on(/wie ben jij/, function (msg) {
+  return bot.sendMessage(msg.from.id, "Ik ben een vervelend bolletje uit Star Wars.");
+});
+bot.on(/je moeder/, function (msg) {
+  return bot.sendMessage(msg.from.id, "Is vast een hele lieve vrouw!");
+});
+bot.on(/Joli-Coeur/, function (msg) {
+  return bot.sendMessage(msg.from.id, "Hij is cool!");
+});
+
+bot.on(/(.+)/, function (msg, props) {
+  var text = props.match[1];
+  console.log(msg.from.first_name + " " + msg.from.last_name + ": " + text);
+});
+
+bot.start();
